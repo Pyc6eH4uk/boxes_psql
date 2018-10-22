ea1b42e7aa6560c3966b8d5fdadeaba62bc25155
pyc6eh4uk <ruslan.klimov1@gmail.com>
2018-10-22 12:08:28 +0300

    Fixiki

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = os.urandom(24)
 
 
 def connect_db():
-    return psycopg2.connect('postgres://postgres:user@localhost:5432/postgres')
+    return psycopg2.connect('postgres://postgres:user@localhost:5432/pyc6eh4uk')
 
 
 @app.before_request
@@ -18,15 +18,12 @@ def before_request():
 
 @app.route('/')
 def index():
-    print('here')
     if 'user' in session:
         cur = g.db_conn.cursor()
         cur.execute('SELECT box_name FROM boxes')
         boxes = cur.fetchall()
-        return str(boxes)
-        # return render_template('index.html', boxes=boxes)
+        return render_template('index.html', boxes=boxes)
     else:
-        # return render_template('login.html')
         return redirect(url_for('login'))
 
 
@@ -42,7 +39,8 @@ def login():
         cur.execute('SELECT * FROM box_users WHERE user_login = %s AND user_password = %s', (email, pwd,))
         data = cur.fetchall()
         if not data:
-            return render_template('login.html')
+            error = 'Login or password incorrect'
+            return render_template('login.html', error=error)
 
         session['user'] = request.form['name']
         print(session)
@@ -67,34 +65,24 @@ def available_boxes():
             cur = g.db_conn.cursor()
             cur.execute('SELECT user_id FROM box_users WHERE user_login = %s', (session['user'], ))
             user_id = cur.fetchone()[0]
-            print(user_id)
             cur.execute('SELECT box_name FROM boxes')
             boxes = cur.fetchall()
-            print(boxes)
             if color not in colors:
                 error = 'You can not add box with such color: {}'.format(color)
-                return error
-                # return render_template('boxes.html', boxes=boxes, error=error)
+                return render_template('boxes.html', boxes=boxes, error=error)
             cur.execute('SELECT box_color FROM boxes WHERE box_color = %s', (color, ))
             if cur.fetchall():
-               return 'Takoi est'
+                error = 'You can not add box with such name: {}'.format(name)
+                return render_template('boxes.html', boxes=boxes, error=error)
             cur.execute('SELECT * FROM boxes WHERE box_name = %s OR box_color = %s', (name, color, ))
             data = cur.fetchall()
             if not data:
-                print('create')
                 create_box(name, color, user_id)
                 cur.execute('SELECT box_name, box_color FROM boxes')
                 boxes = cur.fetchall()
-                print(boxes)
                 happy = 'We add your box to our storage.'
                 return render_template('boxes.html', boxes=boxes, happy=happy)
-
-            # if color not in boxes.values() and name not in boxes.keys():
-            #     create_box(name, color)
-            #     happy = 'We add your box to our storage.'
-            #     return render_template('boxes.html', boxes=boxes, happy=happy)
             else:
-                print('not_create')
                 error = 'Box with such color or name already exist.'
                 return render_template('boxes.html', boxes=boxes, error=error)
         else:
@@ -104,49 +92,51 @@ def available_boxes():
 @app.route('/boxes/<box_name>', methods=['GET', 'PUT'])
 def extend_box(box_name):
     if request.method == 'GET':
-        if is_box_exist(box_name):
-            cur = g.db_conn.cursor()
-            cur.execute('SELECT box_color, box_id FROM boxes WHERE box_name = %s', (box_name, ))
-            box_data = cur.fetchone()
-            cur.execute('SELECT thing_name FROM box_things WHERE box_id = %s', (box_data[1], ))
-            things = cur.fetchall()
-            new_things = []
-            for thing in things:
-                new_things.append(thing[0])
-            new_things = sorted(new_things)
-            cnt_things = []
-            for thing in set(new_things):
-                cnt_things.append(new_things.count(thing))
-            all_info = []
-            for thing, cnt in zip(set(sorted(new_things)), cnt_things):
-                all_info.append([thing, cnt])
-            return render_template('box_items.html', box_name=box_name,
-                                   color=box_data[0],  things=sorted(all_info))
+        if 'user' in session:
+            if is_box_exist(box_name):
+                cur = g.db_conn.cursor()
+                cur.execute('SELECT box_color, box_id FROM boxes WHERE box_name = %s', (box_name, ))
+                box_data = cur.fetchone()
+                cur.execute('SELECT thing_name FROM box_things WHERE box_id = %s', (box_data[1], ))
+                things = cur.fetchall()
+                new_things = []
+                for thing in things:
+                    new_things.append(thing[0])
+                new_things = sorted(new_things)
+                cnt_things = []
+                for thing in set(new_things):
+                    cnt_things.append(new_things.count(thing))
+                all_info = []
+                for thing, cnt in zip(set(sorted(new_things)), cnt_things):
+                    all_info.append([thing, cnt])
+                return render_template('box_items.html', box_name=box_name,
+                                       color=box_data[0],  things=sorted(all_info))
 
+            else:
+                return 'No boxes with {} name'.format(box_name), 404
         else:
-            return 'No boxes with {} name'.format(box_name), 404
+            return redirect(url_for('login'))
 
     if request.method == 'PUT':
-        if is_box_exist(box_name):
-            cur = g.db_conn.cursor()
-            cur.execute('SELECT user_id FROM box_users WHERE user_login = %s', (session['user'], ))
-            creator = cur.fetchone()[0]
-            cur.execute('SELECT box_name FROM boxes WHERE box_name = %s AND user_id = %s', (box_name, creator, ))
-            # if session['user'] == creator:
-            if cur.fetchall():
-                name = request.form['name']
-                cur.execute('SELECT box_id FROM boxes WHERE box_name = %s', (box_name, ))
-                box_id = cur.fetchone()[0]
-                extend_boxes(name, box_id)
-                cur.execute('SELECT thing_name FROM box_things WHERE box_id = %s', (box_id,))
-                things = cur.fetchall()
-                cur.execute('SELECT box_color FROM boxes WHERE box_name = %s', (box_name,))
-                color = cur.fetchone()[0]
-                return redirect(url_for('extend_box', box_name=box_name))
+        if 'user' in session:
+            if is_box_exist(box_name):
+                cur = g.db_conn.cursor()
+                cur.execute('SELECT user_id FROM box_users WHERE user_login = %s', (session['user'], ))
+                creator = cur.fetchone()[0]
+                cur.execute('SELECT box_name FROM boxes WHERE box_name = %s AND user_id = %s', (box_name, creator, ))
+                if cur.fetchall():
+                    name = request.form['name']
+                    cur.execute('SELECT box_id FROM boxes WHERE box_name = %s', (box_name, ))
+                    box_id = cur.fetchone()[0]
+                    extend_boxes(name, box_id)
+                    return redirect(url_for('extend_box', box_name=box_name))
+                else:
+                    error = 'Restricted access'
+                    return render_template('box_items.html', error=error), 401
             else:
-                return 'Restricted access', 400
+                return '<h1>No boxes with <b>{}</b> name<h1>'.format(box_name), 404
         else:
-            return 'No boxes with {} name'.format(box_name), 404
+            return redirect(url_for('login'))
 
 
 def is_box_exist(box_name):
@@ -179,3 +169,4 @@ def extend_boxes(name, box_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
